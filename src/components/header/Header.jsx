7855e6165257a35d0c03bd6fdccef3865017272f
dani <danifromecuador@gmail.com>
2025-01-13 18:01:39 -0500

    Avoid using color argument as inline style and instead using color properties in the css external file

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,10 +8,10 @@ export const Header = () => {
   return (
     <nav className='header'>
       <ul>
-        <li><a href='#home'><FaHouse className='mobile' color='bisque' /><span className='desktop'>Home</span></a></li>
-        <li><a href='#projects'><FaBriefcase className='mobile' color='bisque' /><span className='desktop'>Projects</span></a></li>
-        <li><a href='#about'><FaIdCard className='mobile' color='bisque' /><span className='desktop'>About</span></a></li>
-        <li><a href='#contact'><FaPhone className='mobile' color='bisque' /><span className='desktop'>Contact</span></a></li>
+        <li><a href='#home'><FaHouse className='mobile' /><span className='desktop'>Home</span></a></li>
+        <li><a href='#projects'><FaBriefcase className='mobile' /><span className='desktop'>Projects</span></a></li>
+        <li><a href='#about'><FaIdCard className='mobile' /><span className='desktop'>About</span></a></li>
+        <li><a href='#contact'><FaPhone className='mobile' /><span className='desktop'>Contact</span></a></li>
       </ul>
     </nav>
   )

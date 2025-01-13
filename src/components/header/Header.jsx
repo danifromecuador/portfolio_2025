f683b18678614245741b11b987c65d116936b7ef
dani <danifromecuador@gmail.com>
2025-01-13 01:29:50 -0500

    Toggle between texts and icons for desktop or mobile layouts feat/navbar-and-main-components

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,17 @@
 import './Header.css'
+import { FaHouse } from "react-icons/fa6";
+import { FaBriefcase } from "react-icons/fa";
+import { FaIdCard } from "react-icons/fa";
+import { FaPhone } from "react-icons/fa";
 
 export const Header = () => {
   return (
     <nav className='header'>
       <ul>
-        <li><a href='#home'>Home</a></li>
-        <li><a href='#projects'>Projects</a></li>
-        <li><a href='#about'>About</a></li>
-        <li><a href='#contact'>Contact</a></li>
+        <li><a href='#home'><FaHouse className='mobile' /><span className='desktop'>Home</span></a></li>
+        <li><a href='#projects'><FaBriefcase className='mobile' /><span className='desktop'>Projects</span></a></li>
+        <li><a href='#about'><FaIdCard className='mobile' /><span className='desktop'>About</span></a></li>
+        <li><a href='#contact'><FaPhone className='mobile' /><span className='desktop'>Contact</span></a></li>
       </ul>
     </nav>
   )

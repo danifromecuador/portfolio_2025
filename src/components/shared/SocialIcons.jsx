5d7e05ade582b9ece86a6ceaf1fe761ffb5f8e71
dani <danifromecuador@gmail.com>
2025-01-15 14:19:59 -0500

    Show or hide text info when hovering on social icons feat/home

--- src/components/shared/SocialIcons.jsx
+++ src/components/shared/SocialIcons.jsx
@@ -3,14 +3,23 @@ import { FaLinkedin } from "react-icons/fa";
 import { FaGithubSquare } from "react-icons/fa";
 import { FaEnvelopeSquare } from "react-icons/fa";
 import { FaWhatsappSquare } from "react-icons/fa";
+import { useState } from 'react';
 
 export const SocialIcons = () => {
+  const [s1, ss1] = useState(false)
+  const [s2, ss2] = useState(false)
+  const [s3, ss3] = useState(false)
+  const [s4, ss4] = useState(false)
   return (
     <ul className='social-icons'>
-      <a href="https://www.linkedin.com/in/danifromec/" target='_blank' rel='noopener'><FaLinkedin /></a>
-      <a href="https://github.com/danifromecuador" target='_blank' rel='noopener'><FaGithubSquare /></a>
-      <a href="mailto:danifromecuador@gmail.com" target='_blank' rel='noopener'><FaEnvelopeSquare /></a>
-      <a href="https://wa.me/593980022515" target='_blank' rel='noopener'><FaWhatsappSquare /></a>
+      <a href="https://www.linkedin.com/in/danifromec/" target='_blank' rel='noopener' onMouseEnter={() => ss1(true)} onMouseLeave={() => ss1(false)}><FaLinkedin /></a>
+      <span className={`${s1 || 'hide'}`}>@danifromec</span>
+      <a href="https://github.com/danifromecuador" target='_blank' rel='noopener' onMouseEnter={() => ss2(true)} onMouseLeave={() => ss2(false)}><FaGithubSquare /></a>
+      <span className={`${s2 || 'hide'}`}>@danifromecuador</span>
+      <a href="mailto:danifromecuador@gmail.com" target='_blank' rel='noopener' onMouseEnter={() => ss3(true)} onMouseLeave={() => ss3(false)}><FaEnvelopeSquare /></a>
+      <span className={`${s3 || 'hide'}`}>danifromecuador@gmail.com</span>
+      <a href="https://wa.me/593980022515" target='_blank' rel='noopener' onMouseEnter={() => ss4(true)} onMouseLeave={() => ss4(false)}><FaWhatsappSquare /></a>
+      <span className={`${s4 || 'hide'}`}>+593980022515</span>
     </ul>
   )
 }

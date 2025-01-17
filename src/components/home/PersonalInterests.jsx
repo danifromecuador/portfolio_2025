6f7a3a4ee7e15f79720a2ca7079b8bf70cbc52f2
dani <danifromecuador@gmail.com>
2025-01-16 19:22:50 -0500

    Render personal interests data feat/home

---
+++ src/components/home/PersonalInterests.jsx
@@ -0,0 +1,13 @@
+import './PersonalInterests.css'
+
+export const PersonalInterests = () => {
+  return (
+    <div className='personal-interests'>
+      <img src="" alt="" />
+      <div>
+        <h3>Reading books</h3>
+        <p>I've read tons of books in spanish and recently I started reading in English</p>
+      </div>
+    </div>
+  )
+}
